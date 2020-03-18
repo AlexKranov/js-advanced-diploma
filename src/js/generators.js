80407a7fad144a2fca74ea4296d81efcfc4e434c
Alexander Kresnov <kresnov@gmail.com>
2020-03-18 15:40:01 +0300

    от 18.03 правки

--- src/js/generators.js
+++ src/js/generators.js
@@ -6,9 +6,20 @@
  * @returns Character type children (ex. Magician, Bowman, etc)
  */
 export function* characterGenerator(allowedTypes, maxLevel) {
-  // TODO: write logic here
+  while (true) {
+    const typePlayer = Math.floor(Math.random() * allowedTypes.length);
+    const level = Math.floor(Math.random() * maxLevel) + 1;
+    yield new allowedTypes[typePlayer](level);
+  }
 }
 
 export function generateTeam(allowedTypes, maxLevel, characterCount) {
   // TODO: write logic here
+  const hero = characterGenerator(allowedTypes, maxLevel);
+  const playerTeam = [];
+
+  for (let i = 0; i < characterCount; i += 1) {
+    playerTeam.push(hero.next().value);
+  }
+  return playerTeam;
 }
